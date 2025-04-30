0ba279e99b712be33c49372ae755240ffb16bd94
Fabricio Schiavon Kolberg <gretgor42@gmail.com>
2025-04-30 20:56:14 -0300

    feat(shor_factoring.py): modify the implementation of the main factoring algorithm to add dynamic programming tables for both the decompositions and the modular orders

--- shor_factoring.py
+++ shor_factoring.py
@@ -8,6 +8,8 @@ import numpy as np
 from math import gcd, floor, log
 from fractions import Fraction
 import random
+from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+from qiskit_ibm_runtime import QiskitRuntimeService, SamplerV2 as Sampler
 
 def mod_mult_gate(b,N):
     """
@@ -75,85 +77,119 @@ def order_finding_circuit(a,N):
 
     return circuit
 
-def find_order_classical(a, N):
-    """
-    Finds the order of a in Z_N. a and N must be mutually prime
-    """
-    cur_exp = 1
-    while pow(a,cur_exp,N) != 1:
-        cur_exp += 1
-    return cur_exp
 
-def find_order(a, N, classical=False):
-    """
-    Given a, N, mutually prime and such that a < N, applies the quantum
-    order finding circuit to obtain the order of a in Z_N.
+class IntegerFactoring:
+    def __init__(self):
+        self.dynamic_table = {}
+        self.order_table = {}
+
+    def find_order_classical(self, a, N):
+        """
+        Finds the order of a in Z_N. a and N must be mutually prime
+        """
+        cur_exp = 1
+        while pow(a,cur_exp,N) != 1:
+            cur_exp += 1
+        self.order_table[(a,N)] = cur_exp
+        return cur_exp
+
+    def find_order(self, a, N, classical=False, real_cloud=True):
+        """
+        Given a, N, mutually prime and such that a < N, applies the quantum
+        order finding circuit to obtain the order of a in Z_N.
+        
+        Set flag "classical" for classical order finding
+        """
+        if (a,N) in self.order_table.keys():
+            return self.order_table[(a,N)]
+        if classical:
+            print(f"finding order for {a},{N} in classical way", file=sys.stderr)
+            return self.find_order_classical(a, N)
+        n = floor(log(N-1,2)) + 1
+        m = 2*n
+        circuit = order_finding_circuit(a,N)
+
+        # loops indefinitely until it finds the order.
+        # does not take as long as it seems, since the probability of finding
+        # the correct order is very high.
+        transpiled_circuit = transpile(circuit,AerSimulator())
+        while not real_cloud:
+            result = AerSimulator().run(
+                transpiled_circuit,
+                shots=1,
+                memory=True).result()
+            y = int(result.get_memory()[0],2)
+            r = Fraction(y/2**m).limit_denominator(N).denominator
+            if pow(a,r,N)==1: 
+                self.order_table[(a,N)] = r
+                return r
+
+        # if real_cloud is set to true, use the IBM cloud for actual quantum computing
+        service = QiskitRuntimeService()
+        backend = service.least_busy(operational=True, simulator=False)
+        pm = generate_preset_pass_manager(backend=backend, optimization_level=1)
+        circuit_to_run = pm.run(circuit)
+
+        sampler = Sampler(mode=backend)
+        sampler.options.default_shots = 1024
+        job = sampler.run([circuit_to_run])
+        my_result = job.result()[0].data.Z.array
+        for r in my_result:
+            if pow(a,int(r[0]),N)==1: 
+                self.order_table[(a,N)] = int(r[0])
+                return int(r[0])
+
+
+    def factor_integer(self, N, classical=False):
+        """ 
+        Breaks an integer into prime factors, and returns the factors as a list
     
-    Set flag "classical" for classical order finding
-    """
-    if classical:
-        return find_order_classical(a, N)
-    n = floor(log(N-1,2)) + 1
-    m = 2*n
-    circuit = order_finding_circuit(a,N)
-    transpiled_circuit = transpile(circuit,AerSimulator())
-
-    # loops indefinitely until it finds the order.
-    # does not take as long as it seems, since the probability of finding
-    # the correct order is very high.
-    while True:
-        result = AerSimulator().run(
-            transpiled_circuit,
-            shots=1,
-            memory=True).result()
-        y = int(result.get_memory()[0],2)
-        r = Fraction(y/2**m).limit_denominator(N).denominator
-        if pow(a,r,N)==1: 
-            return r
-
-def factor_integer(N, classical=False):
-    """ 
-    Breaks an integer into prime factors, and returns the factors as a list
+        args: N: integer to factor
+        returns: list of prime factors
+        """
+        # with a failure chance of less than 1/2, we can safely assume, with less than 0.1% probability of mistake,
+        # that any number for which we fail to find a non-trivial factor after trying 10 times is, itself, prime
+        MAX_ATTEMPTS = 10
     
-    args: N: integer to factor
-    returns: list of prime factors
-    """
-    # with a failure chance of less than 1/2, we can safely assume, with less than 0.1% probability of mistake,
-    # that any number for which we fail to find a non-trivial factor after trying 10 times is, itself, prime
-    MAX_ATTEMPTS = 10
-
-    if N == 1:
-        return []
-    if N % 2 == 0:
-        # N is even, extract the 2 and make a recursive call
-        return [2] + factor_integer(N//2)
+        if N == 1:
+            return []
+        # if the factoring for N has already been computed, just use it
+        if N in self.dynamic_table.keys():
+            return self.dynamic_table[N]
+        if N % 2 == 0:
+            # N is even, extract the 2 and make a recursive call
+            self.dynamic_table[N] = [2] + self.factor_integer(N//2, classical)
+            return self.dynamic_table[N]        
     
-    # check if the number is a power (if so, iterate over the power)
-    for k in range(2,round(log(N,2))+1):
-        d = int(round(N ** (1/k)))
-        if d**k == N:
-            # Number is a power, iterate on the root
-            return factor_integer(d) + factor_integer(N//d)
-    factor = -1
-    num_its = 0
-    while factor == -1 and num_its < MAX_ATTEMPTS:
-        a = random.randint(2,N-1)
-        d = gcd(a,N)
-        if d > 1:
-            factor = d
+        # check if the number is a power (if so, iterate over the power)
+        for k in range(2,round(log(N,2))+1):
+            d = int(round(N ** (1/k)))
+            if d**k == N:
+                # Number is a power, iterate on the root
+                self.dynamic_table[N] = self.factor_integer(d, classical) + self.factor_integer(N//d, classical)
+                return self.dynamic_table[N]
+        factor = -1
+        num_its = 0
+        while factor == -1 and num_its < MAX_ATTEMPTS:
+            a = random.randint(2,N-1)
+            d = gcd(a,N)
+            if d > 1:
+                factor = d
+            else:
+                r = self.find_order(a, N, classical)
+                if r % 2 == 0:
+                    x = pow(a,r//2,N) - 1
+                    d = gcd(x,N)
+                    if d > 1: 
+                        factor = d
+            num_its += 1
+        if factor > -1:
+            self.dynamic_table[N] = self.factor_integer(d, classical) + self.factor_integer(N//d, classical)
+            return self.dynamic_table[N]
         else:
-            r = find_order(a, N, classical)
-            if r % 2 == 0:
-                x = pow(a,r//2,N) - 1
-                d = gcd(x,N)
-                if d > 1: 
-                    factor = d
-        num_its += 1
-    if factor > -1:
-        return factor_integer(d) + factor_integer(N//d)
-    else:
-        # if no factor was ever found, then we can assume N itself is prime
-        return [N]
+            # if no factor was ever found, then we can assume N itself is prime
+            self.dynamic_table[N] = [N]
+            return [N]
 
 def show_factoring(N, classical=False):
     """
@@ -163,7 +199,8 @@ def show_factoring(N, classical=False):
           classical: set for classical order finding (i.e. not via quantum circuit)
     Returns: a string containing a human-readable form of the prime decomposition
     """
-    factor_list = factor_integer(N, classical)
+    factorer = IntegerFactoring()
+    factor_list = factorer.factor_integer(N, classical)
     factor_list.sort()
     cur_factor = -1
     cur_exponent = 0
@@ -188,6 +225,7 @@ def show_factoring(N, classical=False):
 if __name__ == '__main__':
     number_to_factor = int(sys.argv[1])
     classical = len(sys.argv) > 2 and sys.argv[2] == '1'
+    print(f"classical: {classical}", file=sys.stderr)
     print(f"Prime decomposition of number {number_to_factor}:")
     print(show_factoring(number_to_factor, classical))
     
